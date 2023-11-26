1c9110f6c710a5e6aad7936a344c5aa6dfaedb19
karit <hearer2028@edu.spbstu.ru>
2023-11-26 19:44:24 +0300

    Лабораторная работа номер 2

--- Функции и методы/Лабораторная работа/Поиск индекса элемента в списке товаров/main.py
+++ Функции и методы/Лабораторная работа/Поиск индекса элемента в списке товаров/main.py
@@ -1,15 +1,23 @@
 # TODO Напишите функцию для поиска индекса товара
-def find_index(spisok,tovar):
-    if tovar in spisok:
-        return spisok.index(tovar)
+def find_index(spisok, tovar):
+    try:
+        elem_idx = spisok.index(tovar)
+    except ValueError:
+        print(f"Товар '{tovar}' не найден в списке.")
     else:
-        return None
+        print(f"Первое вхождение товара '{tovar}' имеет индекс {elem_idx}.")
+
+
 
 items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+find_index(items_list, 'банан')
+find_index(items_list, 'груша')
+find_index(items_list, 'персик')
 
-for find_item in ['банан', 'груша', 'персик']:
-    index_item = find_index(items_list,find_item)   # TODO Вызовите функцию, что получить индекс товара
-    if index_item is not None:
-        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
-    else:
-        print(f"Товар '{find_item}' не найден в списке.")
+
+#for find_item in ['банан', 'груша', 'персик']:
+ #   index_item = find_index(items_list,find_item)   # TODO Вызовите функцию, что получить индекс товара
+  #  if index_item is not None:
+   #     print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    #else:
+     #   print(f"Товар '{find_item}' не найден в списке.")
